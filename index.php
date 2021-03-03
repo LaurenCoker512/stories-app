1e6d4f35044135203cb59ae655cbb4f0243cb515
Lauren Coker <laurenekcoker@gmail.com>
2021-03-03 09:31:47 -0600

    Initial Laravel setup with default auth

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Build your features using Test First Development. Don't write any feature code until the failing test is created first. 
+        // 1) Write the test; 2) Write code to make it pass; 3) Refactor; 4) Rinse & Repeat.
+    // Integrate user authentication (you can use session or JWT authentication).
+    // Integrate sending email (with HTML and Plain Text versions of the email).
+    // All models should have validation. Between all models, make sure to use ALL of these validation rules at least once:
+
+?>
